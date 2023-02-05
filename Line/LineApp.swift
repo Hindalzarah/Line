dc9ba8fcff25fe3bc607082d9f8a68f99efdbdd2
Hind <hindalzarah@outlook.com>
2023-02-05 12:56:07 +0300

    Initial Commit

---
+++ Line/LineApp.swift
@@ -0,0 +1,17 @@
+//
+//  LineApp.swift
+//  Line
+//
+//  Created by Hind on 14/07/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct LineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
